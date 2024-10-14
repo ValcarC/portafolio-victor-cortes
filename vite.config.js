35dacfa9d60661c05c18e8d63a1946503dce1f6e
ValcarC <valcar.cortes@gmail.com>
2024-10-14 14:32:27 -0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Asegúrate de que el base path sea correcto
-  build: {
-    outDir: 'dist', // Directorio de salida para la construcción
-  },
-});
+})
